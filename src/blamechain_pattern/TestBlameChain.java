933f3e2ea41aec1ff4f6ef6d3a037e170c6d004c
liguozhang <364627306@qq.com>
2022-11-10 10:48:32 +0800

    add file and add message

--- src/blamechain_pattern/TestBlameChain.java
+++ src/blamechain_pattern/TestBlameChain.java
@@ -2,6 +2,12 @@ package blamechain_pattern;
 
 public class TestBlameChain {
 
+    /**
+     * 责任链模式是一种创建处理对象序列（比如操作序列）的通用方案。一个处理对象可能需要
+     * 在完成一些工作之后，将结果传递给另一个对象，这个对象接着做一些工作，再转交给下一个处
+     * 理对象，以此类推。
+     * @param args
+     */
     public static void main(String[] args) {
         Leader leader1 = new Teacher();
         Leader leader2 = new Director();
